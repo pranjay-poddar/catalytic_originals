f4ff5e3b416b25bf37744dae9fe22d45b5667834
Pranjay Poddar <52539501+pranjay-poddar@users.noreply.github.com>
2020-09-28 20:49:40 +0530

    Add files via upload

---
+++ catalytic.js
@@ -0,0 +1,27 @@
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
+  function greet()
+  {
+      let name = document.querySelector('#name').value;
+      alert('Hello, ' + name + '!' + ' ' + 'Welcome to Catalytic Originals Official Page '+'.');
+  }
