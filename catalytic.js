35b4fbad717317d27a1f6977ff1d343f1a801ba9
Pranjay Poddar <52539501+pranjay-poddar@users.noreply.github.com>
2020-10-16 19:19:31 +0530

    Update catalytic.js

--- catalytic.js
+++ catalytic.js
@@ -12,3 +12,18 @@ function myFunction() {
       let name = document.querySelector('#name').value;
       alert('Hello, ' + name + '!' + ' ' + 'Welcome to Catalytic Originals Official Page '+'.');
   }
+
+  function darkmode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    if(document.getElementById("dark").innerHTML=="Dark Mode")
+    document.getElementById("dark").innerHTML="Light Mode";
+    else
+    document.getElementById("dark").innerHTML="Dark Mode";
+
+
+    if(document.getElementById("darknav").innerHTML=="Dark Mode")
+    document.getElementById("darknav").innerHTML="Light Mode";
+    else
+    document.getElementById("darknav").innerHTML="Dark Mode";
+  }
